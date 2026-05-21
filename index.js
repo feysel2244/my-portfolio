13b57eaf3af87afe07124e8dd2ad4a7641b777e7
unknown <feyselanwar2244@gmail.com>
2026-05-21 19:06:54 +0800

    toggle top button added

--- index.js
+++ index.js
@@ -132,3 +132,20 @@ document.addEventListener('click', (e) => {
     menuBtn.setAttribute('aria-expanded', false)
   }
 })
+const scrollTopBtn = document.getElementById('scroll-top')
+
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 400) {
+    scrollTopBtn.classList.add('visible')
+  } else {
+    scrollTopBtn.classList.remove('visible')
+  }
+})
+
+scrollTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
+})
